7a79a10674aef9dda17e84526d3806309fac2352
Daniel_Asulin_2025_Katza <danielmelet15@gmail.com>
2025-03-02 16:22:41 +0200

    גרסה ראשונה

--- app/src/main/java/com/daniel/myfirstapplication/MainActivity.java
+++ app/src/main/java/com/daniel/myfirstapplication/MainActivity.java
@@ -1,18 +1,24 @@
 package com.daniel.myfirstapplication;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public abstract class MainActivity extends AppCompatActivity {
 
     Button btn1, btn2;
     @Override
@@ -47,4 +53,30 @@ public class MainActivity extends AppCompatActivity {
         } else if (view == btn2) {
         }//
      }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        super.onOptionsItemSelected(item);
+        int id = item.getItemId();
+        if(id == R.id.action_login){
+            Toast.makeText(this, "you selected login", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.action_register){
+            Toast.makeText(this, "you selected register", Toast.LENGTH_SHORT).show();
+        }
+        else if(id == R.id.action_start){
+            Toast.makeText(this, "you selected start", Toast.LENGTH_SHORT).show();
+        }
+        else if(item.getItemId() == R.id.new_item) {
+            Intent intent = new Intent(this, NewActivity.class);
+            startActivity(intent);
+        }
+        return true;
+    }
+
 }
